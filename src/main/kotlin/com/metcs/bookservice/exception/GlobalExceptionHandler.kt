38f98ce2c0ad8b492c8aa1569bc1f967a3457e87
Mustafa Enes Tepe <mustafatpe23@gmail.com>
2023-07-06 08:37:20 +0300

    Global Exception Handler added

---
+++ src/main/kotlin/com/metcs/bookservice/exception/GlobalExceptionHandler.kt
@@ -0,0 +1,22 @@
+package com.metcs.bookservice.exception
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.context.request.ServletWebRequest
+import org.springframework.web.context.request.WebRequest
+
+@RestControllerAdvice
+class GlobalExceptionHandler(
+    val exceptionUtil: ExceptionUtil
+) {
+    @ExceptionHandler
+    fun handleGeneralException(ex: Exception,request: WebRequest):ResponseEntity<GeneralExceptionMessage>{
+        val servletRequest = request as ServletWebRequest
+        val uri = exceptionUtil.getFullUri(servletRequest.request)
+        val errorMessage=GeneralExceptionMessage(ex.message,uri,HttpStatus
+            .INTERNAL_SERVER_ERROR.value())
+        return ResponseEntity(errorMessage,HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+}
